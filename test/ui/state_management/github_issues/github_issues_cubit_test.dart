9887de92d1a16d3d9220da28c888b04d493fa6fc
ramin-vakili <ramin.vaki@gmail.com>
2022-04-21 22:23:46 +0430

    Adding GithubIssuesCubit test

--- test/ui/state_management/github_issues/github_issues_cubit_test.dart
+++ test/ui/state_management/github_issues/github_issues_cubit_test.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:media_market_challenge/domain/models/issue.dart';
+import 'package:media_market_challenge/domain/repositories/issues_repository.dart';
+
+class MockGithubIssues implements IssuesRepository {
+  @override
+  Future<List<Issue>> getIssues({
+    String repoName = 'mock_repo',
+    String? repoOwner,
+  }) {
+    if (repoName == 'mock_repo') {
+      return Future.value(
+        <Issue>[],
+      );
+    }
+
+    throw FlutterError('Repo not found!');
+  }
+}
+
+void main() {
+  group('GithubIssues cubit', () {});
+}
