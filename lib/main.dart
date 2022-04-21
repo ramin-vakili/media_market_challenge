ff96b28feb3e005156885943885a866edc237c2a
ramin-vakili <ramin.vaki@gmail.com>
2022-04-21 18:02:24 +0430

    Move home_page file to the correct folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ui/pages/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,27 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MediaMarktChallenge'),
-      ),
-      body: const Center(
-        child: Text('Sample text'),
-      ),
     );
   }
 }
